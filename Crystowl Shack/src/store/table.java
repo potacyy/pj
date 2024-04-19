f3a2e388b7ab9860287e5f71951d98b05f036567
potacyy <145527861+potacyy@users.noreply.github.com>
2024-04-20 07:19:43 +0800

    Add files via upload

---
+++ Crystowl Shack/src/store/table.java
@@ -0,0 +1,15 @@
+package pjstore;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+import javax.swing.JTable;
+public class table extends JTable {
+    
+}
